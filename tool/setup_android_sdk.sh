60996ca424d5eae812aa6b14d04ba8a8218ec28d
Henry Tao <hi@henrytao.me>
2015-04-06 06:05:21 +0700

    [SETUP-SHIPPABLE] Fixed build tools

--- tool/setup_android_sdk.sh
+++ tool/setup_android_sdk.sh
@@ -4,10 +4,17 @@ sudo apt-get update -qq
 if [ `uname -m` = x86_64 ]; then
   sudo apt-get install -qq --force-yes libgd2-xpm ia32-libs ia32-libs-multiarch lib32stdc++6 lib32z1 > /dev/null;
 fi
+
 wget http://dl.google.com/android/android-sdk_r24.1.2-linux.tgz
-tar -zxf android-sdk_r24.1.2-linux.tgz android-sdk-linux/
+tar -zxf android-sdk_r24.1.2-linux.tgz
+
 export ANDROID_HOME=`pwd`/android-sdk-linux
-export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
-echo y | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter "tools,platform-tools"
-echo y | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter "android-22,build-tools-22.0.1"
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+android list sdk --all --extended
+FILTER=tools,platform-tools,extra-android-support,build-tools-22.0.1,android-22
+yes | android update sdk --no-ui --all --filter $FILTER
+( sleep 5 && while [ 1 ]; do sleep 1; echo y; done ) | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter ${FILTER}
+
 echo "sdk.dir=$ANDROID_HOME" > local.properties
