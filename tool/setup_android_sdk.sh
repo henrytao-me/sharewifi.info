d75384e04c9badf702dbb6e7936ffcec0b107928
Henry Tao <hi@henrytao.me>
2015-04-06 08:37:42 +0700

    [SETUP-SHIPPABLE] Fixed build tools

--- tool/setup_android_sdk.sh
+++ tool/setup_android_sdk.sh
@@ -12,8 +12,21 @@ export ANDROID_HOME=`pwd`/android-sdk-linux
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
-echo y | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter "tools,platform-tools"
-echo y | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter "android-22,build-tools-22.0.0"
-echo y | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter "extra-android-support"
+echo y | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter \
+  tools, \
+  platform-tools, \
+  build-tools-22.0.0
+
+echo y | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter \
+    extra-android-m2repository,\
+    extra-android-support,\
+    extra-google-admob_ads_sdk,\
+    extra-google-analytics_sdk_v2,\
+    extra-google-google_play_services_froyo,\
+    extra-google-google_play_services,\
+    extra-google-m2repository
+
+echo y | $ANDROID_HOME/tools/android update sdk --no-ui --all --filter \
+  android-22
 
 echo "sdk.dir=$ANDROID_HOME" > local.properties
