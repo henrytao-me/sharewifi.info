f174a9b3ce87f36b791822538a794dc0a825ef1b
Henry Tao <hi@henrytao.me>
2015-04-15 09:14:33 +0700

    [DEVELOP] Updated PersistentSearch

---
+++ app/src/main/java/me/henrytao/sharewifi/util/IntentUtils.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015 "Henry Tao <hi@henrytao.me>"
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.henrytao.sharewifi.util;
+
+import com.orhanobut.logger.Logger;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+
+import java.util.List;
+
+/**
+ * Created by henrytao on 4/15/15.
+ */
+public class IntentUtils {
+
+  public static boolean isAvailable(Context context, Intent intent) {
+    try {
+      PackageManager mgr = context.getPackageManager();
+      List<ResolveInfo> list = mgr.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+      return list.size() > 0;
+    } catch(Exception ex) {
+      //Logger.w(ex);
+    }
+    return false;
+  }
+
+}
